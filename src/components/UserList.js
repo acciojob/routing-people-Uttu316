effa79025bc46e067639063c5c06bb6b17dc8737
Utkarsh Gupta <34169292+Uttu316@users.noreply.github.com>
2025-01-31 22:59:58 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import 'regenerator-runtime/runtime'
 
 function UserList() {
   const [users, setUsers] = useState([]);
